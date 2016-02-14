70b9a5e607c373bfe59c8f25f8da5bba610ab21c
jcervantess4113 <jazmincervantesantiago@gmail.com>
2016-02-14 14:41:32 -0600

    Agregue la descripcion del programa y cambie j por y en mayor

--- mayor.py
+++ mayor.py
@@ -1,14 +1,14 @@
-def major(n1,n2,n3):
+# Este programa calcula el mayor de 3 numeros
+
+def mayor(n1,n2,n3):
     if n1>n2 and n1>n3:
         return n1
     elif n2>n3 and n2>n1:
         return n2
     elif n3>n2 and n3>n1:
-        return n3 
-    
+        return n3
+
 n1 = raw_input('write a number:')
 n2 = raw_input('write a number:')
 n3 = raw_input('write a number:')
-print 'el mayor es:' + major(n1,n2,n3)
-
-    
+print 'el mayor es:' + mayor(n1,n2,n3)
